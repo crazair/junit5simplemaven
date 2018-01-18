135af3c7b8efbd7f913e923d9485e5ef13eb1d44
anosv <crazzyzyzy@rambler.ru>
2018-01-18 23:43:46 +0300

    настройки allure

--- src/test/java/com/anosv/example/SelenideTests.java
+++ src/test/java/com/anosv/example/SelenideTests.java
@@ -1,6 +1,7 @@
 package com.anosv.example;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -8,24 +9,45 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
+
+@Feature("My Feature")
 public class SelenideTests {
 
     static {
         Configuration.browser = "chrome";
         Configuration.reportsFolder = "test-result/reports";
-
-        //Configuration.FileDownloadMode;
     }
 
-
     @Test
+    @TmsLink("1234")
     void testSelenideSimple(){
         open("https://yandex.ru/");
+        firstStep("123123123");
+        firstStep2("dsafasdf");
         $(byId("text")).setValue("какой-то текст!!!!!!!");
         $(byXpath(".//div[@class='search2__button']/button")).click();
-        //screenshot("test.");
-       // $(By.className("search2__button")).click();
-        try{Thread.sleep(3500);}catch (Exception e){};
+        try{Thread.sleep(1000);}catch (Exception e){};
+    }
+
+    @Test
+    @Issue("MYISSUE-1")
+    @Feature("Some feature")
+    @Severity(SeverityLevel.CRITICAL)
+    @Link("ALM...")
+    @TmsLinks({@TmsLink("1"), @TmsLink("2")})
+    void testSelenideSimple2(){
+        open("https://yandex.ru/");
+        $(byId("text")).setValue("какой-то текст!!!!!!!");
+        $(byXpath(".//div[@class='search2__button234']/button")).click();
+        try{Thread.sleep(1000);}catch (Exception e){};
+    }
+
+    @Step("Open {0} page.")
+    public void firstStep(String xxx) {
+    }
+
+    @Step("Open {0} page.")
+    public void firstStep2(String yyy) {
     }
 
 }
